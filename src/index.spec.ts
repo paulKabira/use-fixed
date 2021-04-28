ed6ee03f4baef0d9d586428b12aeffd727db19b2
paulKabira <moreakash77@hotmail.com>
2021-04-28 16:26:59 +0530

    feat: Provisions for concurrent mode

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,6 @@
 import { act, renderHook } from '@testing-library/react-hooks/native';
 import test from 'ava';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import { useFixedCallback, useFixedGetter } from './index';
 
@@ -13,7 +13,7 @@ const useTestForFixedCallback = () => {
 const useTestForFixedGetter = () => {
   const [counter, setCounter] = useState(0);
   const getCounter = useFixedGetter(counter);
-  return { getCounter, setCounter };
+  return { getCounter, setCounter, counter };
 };
 
 test('useFixedCallback should return the same reference.', t => {
@@ -47,16 +47,30 @@ test(
   'useFixedGetter should return the same reference even if the data changes,' +
     ' and always return the latest value',
   t => {
-    const { result, rerender } = renderHook(useTestForFixedGetter);
+    const { result } = renderHook(useTestForFixedGetter);
     const getterRef = result.current.getCounter;
 
-    rerender();
     act(() => result.current.setCounter(20));
 
     t.is(result.current.getCounter, getterRef, 'Getter reference should not change');
     t.is(result.current.getCounter(), 20, 'Latest value should be returned.');
   }
 );
+
+test('useFixedGetter should return the latest value in render phase', t => {
+  const { result, rerender } = renderHook(() => {
+    const { getCounter, setCounter, counter } = useTestForFixedGetter();
+    const alternateCounter = useMemo(() => getCounter.n() + 2, [counter]);
+    return { alternateCounter, setCounter, getCounter };
+  });
+  rerender();
+  act(() => result.current.setCounter(20));
+  t.is(
+    result.current.alternateCounter,
+    22,
+    `'useFixedGetter' not returned latest value during render. expected 22, got ${result.current.alternateCounter}`
+  );
+});
 
 test('useFixedCallback should throw if the latest value is not callable', t => {
   const { result } = renderHook(() => {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { useMemo, useRef } from 'react';
+import { useLayoutEffect, useMemo, useRef } from 'react';
+import { LatestCallback, LatestGetter } from './types';
 
 /**
  * Creates a `getter` for the given value. When this getter is called,
@@ -6,10 +7,23 @@ import { useMemo, useRef } from 'react';
  *
  * @param data The value to create a getter for.
  */
-export function useFixedGetter<T>(data: T): () => T {
-  const ref = useRef<T>(data);
-  ref.current = data;
-  return useMemo(() => () => ref.current, []);
+export function useFixedGetter<T>(data: T): LatestGetter<T> {
+  const getterRef = useRef({ c: data, n: data });
+  // Use layout effect here because, any references to the
+  // latest values should be available in layout effects as
+  // well.
+  useLayoutEffect(() => {
+    // Update values for commit phases.
+    getterRef.current.c = data;
+  });
+  // Update value for render phases.
+  getterRef.current.n = data;
+
+  return useMemo(() => {
+    const currentGetter = () => getterRef.current.c;
+    currentGetter.n = () => getterRef.current.n;
+    return currentGetter;
+  }, []);
 }
 
 /**
@@ -18,16 +32,23 @@ export function useFixedGetter<T>(data: T): () => T {
  *
  * @param callback Callback to convert.
  */
-export function useFixedCallback<T extends (...args: any[]) => any>(callback: T): T {
-  const inject = useFixedGetter(callback);
-  return useMemo(() => ((...args) => checkCallback(inject())(...args)) as T, []);
+export function useFixedCallback<T extends (...args: any[]) => any>(
+  callback: T
+): LatestCallback<T> {
+  const getLatestCb = useFixedGetter(callback);
+  return useMemo(() => {
+    // Patch the callback to execute, instead of get the latest value.
+    const patchedCb = (...a: Parameters<T>) => checkCallback(getLatestCb())(...a);
+    patchedCb.n = (...a: Parameters<T>) => checkCallback(getLatestCb.n())(...a);
+    return patchedCb as LatestCallback<T>;
+  }, []);
 }
 
 /**
  * Check if the given value is a callable and return the same. If not,
  * then throw an error.
  */
-function checkCallback(callable: any): CallableFunction {
+function checkCallback<T extends (...args: any[]) => any>(callable: T): T {
   if (typeof callable !== 'function')
     throw new Error('The parameter of useFixedCallback has to be a callable.');
   return callable;
